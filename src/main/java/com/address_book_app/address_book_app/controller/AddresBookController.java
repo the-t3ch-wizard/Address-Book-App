aaaf9cd2baef1876a4379514c8a0e43cbb645757
ayush <ayushmaurya2050@gmail.com>
2025-02-25 18:23:16 +0530

    [Ayush kumar maurya] Add: feature to store data in memory as list

--- src/main/java/com/address_book_app/address_book_app/controller/AddresBookController.java
+++ src/main/java/com/address_book_app/address_book_app/controller/AddresBookController.java
@@ -1,9 +1,13 @@
 package com.address_book_app.address_book_app.controller;
 
+import com.address_book_app.address_book_app.dto.AddressBookDto;
+import com.address_book_app.address_book_app.model.AddressBook;
 import com.address_book_app.address_book_app.service.AddressBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/address-book")
@@ -13,28 +17,28 @@ public class AddresBookController {
     AddressBookService addressBookService;
 
     @GetMapping(value = {"", "/"})
-    public ResponseEntity<String> getAllAddressBook(){
+    public ResponseEntity<List<AddressBook>> getAllAddressBook(){
         return addressBookService.getAllAddressBook();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getAddressBookById(@PathVariable String id){
-        return addressBookService.getAddressBookById(id);
+    @GetMapping("/{name}")
+    public ResponseEntity<AddressBook> getAddressBookByName(@PathVariable String name){
+        return addressBookService.getAddressBookByName(name);
     }
 
     @PostMapping("")
-    public ResponseEntity<String> createAddressBook(){
-        return addressBookService.createAddressBook();
+    public ResponseEntity<AddressBook> createAddressBook(@RequestBody AddressBookDto addressBookDto){
+        return addressBookService.createAddressBook(addressBookDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateAddressBook(@PathVariable String id){
-        return addressBookService.updateAddressBook(id);
+    public ResponseEntity<AddressBook> updateAddressBook(@PathVariable String id, @RequestBody AddressBookDto addressBookDto){
+        return addressBookService.updateAddressBook(Long.parseLong(id), addressBookDto);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAddressBook(@PathVariable String id){
-        return addressBookService.deleteAddressBook(id);
+        return addressBookService.deleteAddressBook(Long.parseLong(id));
     }
 
     @DeleteMapping(value = {"", "/"})

4bc4a3d2197b8ea741e8e0453640fd3a8692ff43
ayush <ayushmaurya2050@gmail.com>
2025-02-27 18:37:37 +0530

    [Ayush kumar maurya] Refactor: remove unnecessary code

--- src/main/java/com/address_book_app/address_book_app/exception/AddressBookNotFoundException.java
+++ src/main/java/com/address_book_app/address_book_app/exception/AddressBookNotFoundException.java
@@ -1,7 +1,5 @@
 package com.address_book_app.address_book_app.exception;
 
-import com.address_book_app.address_book_app.dto.AddressBookDto;
-
 public class AddressBookNotFoundException extends RuntimeException {
 
     public AddressBookNotFoundException(String name) {
